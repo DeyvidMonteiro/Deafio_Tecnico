db30e552446e4a7994a93aaac14e296754251589
Deyvid Monteiro <deyvidmonteiro16@gmail.com>
2025-08-18 12:36:32 -0300

    Adiciona controladores

---
+++ DesafioTecnicoAvanade.EstoqueApi/Controllers/ProductsController.cs
@@ -0,0 +1,86 @@
+﻿using DesafioTecnicoAvanade.EstoqueApi.DTOs;
+using DesafioTecnicoAvanade.EstoqueApi.Services.Product;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioTecnicoAvanade.EstoqueApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _services;
+
+        public ProductsController(IProductService service)
+        {
+            _services = service;
+        }
+
+
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> Get()
+        {
+            var products = await _services.GetProducts();
+
+            if (products is null)
+                return NotFound();
+
+            return Ok(products);
+
+        }
+
+
+        [HttpGet("{id:int}", Name = "GetProducts")]
+        public async Task<ActionResult<CategoryDTO>> Get(int id)
+        {
+            var products = await _services.GetProductById(id);
+
+            if (products is null)
+                return NotFound();
+
+            return Ok(products);
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] ProductDTO productDTO)
+        {
+            if (productDTO is null)
+                return BadRequest("Dados Invalidos");
+
+            await _services.AddProduct(productDTO);
+
+            return new CreatedAtRouteResult("GetProducts", new { id = productDTO.Id }, productDTO);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, [FromBody] ProductDTO productDTO)
+        {
+            if (id != productDTO.Id || productDTO is null)
+                return BadRequest();
+
+            await _services.Updateproduct(productDTO);
+
+            return Ok(productDTO);
+
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var product = await _services.GetProductById(id);
+
+            if (product is null)
+                return NotFound();
+
+            await _services.RemoveProduct(id);
+
+            return Ok(product);
+
+        }
+
+
+    }
+}
