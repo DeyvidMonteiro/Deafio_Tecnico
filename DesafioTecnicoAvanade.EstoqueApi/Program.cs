30e3bf65f9caa4bf75b6002cf6dd21c2e6b7a663
Deyvid Monteiro <deyvidmonteiro16@gmail.com>
2025-08-18 21:36:31 -0300

    adicionando ajustes

--- DesafioTecnicoAvanade.EstoqueApi/Program.cs
+++ DesafioTecnicoAvanade.EstoqueApi/Program.cs
@@ -6,6 +6,8 @@ using DesafioTecnicoAvanade.EstoqueApi.DataAccess.UnitOfWork;
 using DesafioTecnicoAvanade.EstoqueApi.Services.Category;
 using DesafioTecnicoAvanade.EstoqueApi.Services.Product;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,10 +15,38 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers().AddJsonOptions(opt => opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
-    
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "DesafioTecnicoAvanade.EstoqueApi", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"'Bearer' [space] seu token",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new List<string>()
+        }
+
+    });
+});
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
@@ -33,6 +63,28 @@ builder.Services.AddScoped<ICategoryServices, CategoryServices>();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
+
+//Terminar de implementar, lembrar de configurar o appsetings.json
+builder.Services.AddAuthentication("Bearer")
+       .AddJwtBearer("Bearer", options =>
+       {
+           options.Authority = builder.Configuration["IdentityServer:ApplicationUrl"]; // URL do IdentityServer
+           options.TokenValidationParameters = new TokenValidationParameters
+           {
+               ValidateAudience = false
+           };
+       });
+
+builder.Services.AddAuthorization(options =>
+    {
+        options.AddPolicy("ApiScope", policy =>
+        {
+            policy.RequireAuthenticatedUser();
+            policy.RequireClaim("scope", "read"); // substitir pelo escopo que a API vai exigir
+        });
+    });
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -43,6 +95,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
